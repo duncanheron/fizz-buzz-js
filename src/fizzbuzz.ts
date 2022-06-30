979364955c0a38bb05ed05a06ff50778b532d0d0
duncan <duncanuk@gmail.com>
2022-06-30 10:55:57 +0100

    refactors

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export function FizzBuzz(x: number): number | string {
-    if (x % 3 === 0 && x % 5 ===0) return 'FizzBuzz';
-    if (x % 3 === 0) return 'Fizz';
-    if (x % 5 === 0) return 'Buzz';
-    return x;
+    let output = '';
+    if (x % 3 === 0) output += 'Fizz';
+    if (x % 5 === 0) output += 'Buzz';
+    return output.length > 0 ? output : x;
 }
